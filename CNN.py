c76454536303c90884cdd173275b01aa875a875d
zhouzheng <1750931560@qq.com>
2020-12-01 20:23:53 +0800

    git commit -m "second commit"

--- CNN.py
+++ CNN.py
@@ -17,6 +17,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
+import time
 
 torch.set_printoptions(linewidth=120)
 torch.set_grad_enabled(True)
@@ -30,10 +31,10 @@ print(torch.__version__)
 print(torchvision.__version__)
 
 train_set = torchvision.datasets.FashionMNIST(
-    root = 'E:\FashionMnist\FashionMNIST',
-    train = True,
-    download = True,
-    transform = transforms.Compose([transforms.ToTensor()]))
+    root='E:\\allDataSet\\FashionMnist\\FashionMNIST\\FashionMNIST',
+    train=True,
+    download=True,
+    transform=transforms.Compose([transforms.ToTensor()]))
 
 
 class Network(nn.Module):
@@ -63,10 +64,15 @@ def get_num_correct(preds, labels):
 
 
 # 创建网络实例
+# print(torch.cuda.get_device_name(0))
 network = Network()
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# network = network.to(device)
+
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=100)
 optimizer = optim.Adam(network.parameters(), lr=0.01)
-for epoch in range(5):
+time1 = time.time()
+for epoch in range(20):
     total_loss = 0
     total_correct = 0
     for batch in train_loader:  # Get batch
@@ -81,5 +87,6 @@ for epoch in range(5):
         total_correct += get_num_correct(preds, labels)
     print("epoch :", epoch, "loss :", total_loss, "total_correct :", total_correct)
 
-accuracy = total_correct/len(train_set)
-print("accuracy:",accuracy)
+print("timegap :", time.time() - time1)
+accuracy = total_correct / len(train_set)
+print("accuracy:", accuracy)
